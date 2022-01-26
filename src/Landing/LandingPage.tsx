b4e79d9bafd8cffc255b6ccf4f4492d387bf7eea
Junho Yeo <i@junho.io>
2022-01-26 18:35:21 +0900

    Update `LandingPage`

--- src/Landing/LandingPage.tsx
+++ src/Landing/LandingPage.tsx
@@ -5,6 +5,9 @@ import styled from 'styled-components';
 import { useImmutableX } from '@/hooks/useImmutableX';
 import { ETHTokenType } from '@imtbl/imx-sdk';
 
+const AVATAR_URL =
+  'https://lh3.googleusercontent.com/1fQX9jABcIpiU7zjkuIv0H6XmkRZiIlFIQK_7YEzsx8L5Xw2yb0dWXUrtXfQvDuhPG1YRt2BywbstBugUUL7cZgYHg-Xb0XAcVai=w600';
+
 const LandingPage = () => {
   const router = useRouter();
   const [address, setAddress] = useState<string>('');
@@ -30,22 +33,33 @@ const LandingPage = () => {
   return (
     <Wrapper>
       <Container>
-        <Logo src="/logos/diax.svg" alt="DiaX" />
-        <Title>Wallet for Immutable X</Title>
-        <Input
-          placeholder="Ethereum Address"
-          onChange={(e) => {
-            setAddress(e.target.value);
-          }}
-          onKeyDown={(e) => {
-            if (e.key === 'Enter') {
-              router.push(`/${address}`);
-            }
-          }}
-        />
-
-        <Avatar src="https://lh3.googleusercontent.com/1fQX9jABcIpiU7zjkuIv0H6XmkRZiIlFIQK_7YEzsx8L5Xw2yb0dWXUrtXfQvDuhPG1YRt2BywbstBugUUL7cZgYHg-Xb0XAcVai=w600" />
-        <AvatarTitle onClick={onClickDonate}>Donate to me</AvatarTitle>
+        <HeaderSection>
+          <Logo src="/logos/diax.svg" alt="DiaX" />
+          <Title>DiaX, Wallet for Immutable X</Title>
+          <FieldInputContainer>
+            <Field>
+              Search with <strong>Ethereum Address</strong> or{' '}
+              <strong>ENS</strong>
+            </Field>
+            <Input
+              placeholder="0x0000...0000"
+              onChange={(e) => {
+                setAddress(e.target.value);
+              }}
+              onKeyDown={(e) => {
+                if (e.key === 'Enter') {
+                  router.push(`/${address}`);
+                }
+              }}
+            />
+          </FieldInputContainer>
+        </HeaderSection>
+        <AvatarContainer>
+          <Avatar src={AVATAR_URL} />
+        </AvatarContainer>
+        <AlphaDAOImage src="https://avatars.githubusercontent.com/u/92674615?s=200&v=4" />
+        <AvatarName>@Juno</AvatarName>
+        <AvatarTitle onClick={onClickDonate}>Donate 0.1Ξ</AvatarTitle>
       </Container>
     </Wrapper>
   );
@@ -62,10 +76,13 @@ const Wrapper = styled.div`
 const Container = styled.div`
   max-width: 1240px;
   width: 100%;
+  color: white;
+`;
+
+const HeaderSection = styled.section`
   display: flex;
   flex-direction: column;
   align-items: center;
-  color: white;
 `;
 const Logo = styled.img`
   width: 436px;
@@ -75,26 +92,67 @@ const Logo = styled.img`
   }
 `;
 
-const Title = styled.h1``;
+const Title = styled.h1`
+  margin: 0;
+  margin-top: 16px;
 
-const Input = styled.input`
+  font-weight: normal;
+  font-size: 1.25rem;
+`;
+
+const FieldInputContainer = styled.div`
   margin-top: 32px;
   width: 100%;
-  max-width: 450px;
+  max-width: 540px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+const Field = styled.span`
+  width: 100%;
+
+  font-size: 1.25rem;
+  color: #24d1e9;
+`;
+const Input = styled.input`
+  margin-top: 8px;
+  padding: 12px 24px;
+  width: 100%;
+
   border: 3px solid #24d1e9;
   border-radius: 10px;
   background-color: transparent;
-  color: #24d1e9;
-  padding: 12px 24px;
-  font-size: 1.65rem;
+  font-size: 1.25rem;
   line-height: 120%;
+  color: white;
 `;
 
-const Avatar = styled.img`
+const AvatarContainer = styled.div`
   margin-top: 64px;
-  width: 200px;
-  height: 200px;
-  border-radius: 50%;
+  filter: drop-shadow(0px 12px 24px rgba(46, 160, 181, 0.45));
+`;
+const Avatar = styled.img`
+  width: 156px;
+  height: 156px;
+  border-radius: 0;
+  clip-path: polygon(
+    10% 0%,
+    100% 0,
+    100% 0,
+    100% 90%,
+    90% 100%,
+    0 100%,
+    0 100%,
+    0% 10%
+  );
+`;
+const AlphaDAOImage = styled.img`
+  width: 32px;
+  height: 32px;
+`;
+const AvatarName = styled.span`
+  font-weight: bold;
 `;
 const AvatarTitle = styled.span`
   cursor: pointer;
